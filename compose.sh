68bc50e180f5fb1a9c487f9ec8d23d873d3dfeea
rek <Jonathan Rioux>
2019-04-06 20:56:34 -0400

    Rename

--- compose.sh
+++ compose.sh
@@ -11,10 +11,17 @@ sudo docker run --privileged -d -p 8000:8000 --name silverblazerepo $USER/silver
 echo " run : 
 sudo docker exec -it silverblazerepo bash
 cd /srv/workstation-ostree-config
+# A default silverblue ostree :
+
 # Compose the tree with :
 rpm-ostree compose tree --cachedir=/srv/cache --repo=/srv/build-repo /srv/workstation-ostree-config/fedora-silverblue.yaml 
 # Pull repo once commit is done
 ostree --repo=/srv/repo pull-local /srv/build-repo fedora/30/x86_64/silverblue
+
+# A custom silverblaze ostree :
+curl https://raw.githubusercontent.com/johnr14/silverblaze/master/silverblaze.yaml > fedora-silverblaze.yaml
+rpm-ostree compose tree --cachedir=/srv/cache --repo=/srv/build-repo /srv/workstation-ostree-config/fedora-silverblaze.yaml
+ostree --repo=/srv/repo pull-local /srv/build-repo fedora/30/x86_64/silverblaze
 
 # Try it on a running sliverblue ??
 "
